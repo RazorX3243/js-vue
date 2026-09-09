2b734a4a2606dccd70fa48b0743ea35960d63c44
Bohdan <114857272+RazorX3243@users.noreply.github.com>
2026-09-09 11:29:43 +0300

    Add files via upload

---
+++ cw_lesson3/Scripts/js.js
@@ -0,0 +1,65 @@
+// let age=Number(prompt("How old are you?"));
+// let access = confirm("Are you accessed?");
+// if (age>=18 && access===True){
+//     alert("Welcome in");
+// } else{
+//     alert("Access denied.");
+// }
+
+// let role = prompt("What is your access level?");
+// if (role === "admin" || role === "teacher") {
+//     alert("Welcome in");
+// } else{
+//     alert("access denied");
+// }
+
+// let login = confirm("Are you registered?")
+// let age = Number(prompt("Enter your age"))
+//
+// if (login === true) {
+//     if (age >= 18) {
+//         alert("Access allowed")
+//     }
+//     else {
+//         alert("access denied")
+//     }
+// } else {
+//     alert("enter real number")
+// }
+
+// let age = Number(prompt("Enter your age"));
+//
+// if (age >= 18) {
+//     alert("Adult");
+// } else if (age >= 13 && age <= 18) {
+//     alert("Teen");
+// } else if (age >= 0 && age <= 13) {
+//     alert("Kiddo");
+// }
+
+// let age = prompt("What is your age?");
+// if (age >= 18) {
+//     alert(`Access granted`);
+// } else if (age <= 18 && age >= 16) {
+//     let access = confirm("Do you have permission?");
+//     if (access === true) {
+//         alert(`Access granted!`);
+//     } else {
+//         alert(`Get out`);
+//     }
+// }
+
+// let name = prompt("Name of the product");
+// let price = prompt("Price of the product");
+// let quantity = prompt("Quantity of the product");
+// let sign = prompt("Are you signed?");
+// let promo = confirm("Do you have promo?");
+// const promo_w = "sale";
+// let VIP = confirm("status?");
+// const sale1 = 0.9;
+//
+// if (price*quantity >= 1000 && sign && (promo === promo_w || VIP)) {
+//     alert(`Prise with sale :${price*quantity*sale1} uah`);
+// } else {
+//     alert(`No sale price :${price*quantity} uah`);
+// }
